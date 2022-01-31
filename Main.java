a63e31b0c29a3024e30447e07f63727dc1c39db0
Victor Matheron <victor.matheron@gmail.com>
2022-01-31 23:26:38 +0100

    Main.java changed again

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello dear new World !!! This time ok ?... autre essai sans git clone");
+        System.out.println("Hello dear new World of Tor !!! This time ok ?... autre essai sans git clone");
     }
 }
